21d13517fa86ba3c0ca0ce5070471392157a81a5
HasanElakad <hasanosman2005@gmail.com>
2025-11-27 21:57:24 +0200

    tmm

--- src/main/java/com/hotel/management/javafx/App.java
+++ src/main/java/com/hotel/management/javafx/App.java
@@ -1,47 +1,38 @@
 package com.hotel.management.javafx;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
+    public static Scene scene;
 
     @Override
-public void start(Stage stage) throws IOException {
-    Parent root = loadFXML("login");
-    scene = new Scene(root, 640, 480);
+    public void start(Stage stage) throws Exception {
+        Parent root = loadFXML("");          // login.fxml is first
+        scene = new Scene(root, 640, 480);       // size for both screens
+        scene.getStylesheets().add(
+                App.class.getResource("styles.css").toExternalForm()
+        );
+        stage.setScene(scene);
+        stage.setTitle("Hotel Login");
+        stage.show();
+    }
 
-    // load styles.css from same package as App
-    scene.getStylesheets().add(
-            App.class.getResource("styles.css").toExternalForm()
-    );
-
-    stage.setScene(scene);
-    stage.setTitle("Hotel Login");
-    stage.show();
-}
-
-
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws Exception {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    private static Parent loadFXML(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return loader.load();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
